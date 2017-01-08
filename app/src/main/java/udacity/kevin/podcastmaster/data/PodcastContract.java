5099dcf6dc7d2f0a0b8446c95f1b9a4e7f5c92c1
Kevin Gray <Kevin@GoPosse.com>
2017-01-08 02:27:45 -0500

    version 1 of the db contract to be used

--- app/src/main/java/udacity/kevin/podcastmaster/data/PodcastContract.java
+++ app/src/main/java/udacity/kevin/podcastmaster/data/PodcastContract.java
@@ -1,6 +1,7 @@
 package udacity.kevin.podcastmaster.data;
 
 import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
@@ -27,6 +28,18 @@ public class PodcastContract {
 
     // Table name
     public static final String TABLE_NAME = "channels";
+    // Title of the channel, stored as a String
+    public static final String COLUMN_TITLE = "title";
+    // Description of the channel, stored as a String
+    public static final String COLUMN_DESCRIPTION = "description";
+    // URL of the image for the channel, stored as a String
+    public static final String COLUMN_IMAGE_URL = "image_url";
+    // URL of the feed for the channel, stored as a String
+    public static final String COLUMN_FEED_URL = "feed_url";
+
+    public static Uri buildChannelURI(long id) {
+      return ContentUris.withAppendedId(CONTENT_URI, id);
+    }
   }
 
   /* Inner class that defines the table contents of the episodes table */
@@ -41,6 +54,23 @@ public class PodcastContract {
 
     public static final String TABLE_NAME = "episodes";
 
-    
+    // Title of the episode, stored as a String
+    public static final String COLUMN_TITLE = "title";
+    // Pubdate of the episode, stored as a String
+    public static final String COLUMN_PUB_DATE = "pub_date";
+    // Description of the episode, stored as a String
+    public static final String COLUMN_DESCRIPTION = "description";
+    // Duration of the episode, stored as a String
+    public static final String COLUMN_DURATION = "duration";
+    // Enclosure URL of the episode, stored as a String
+    public static final String COLUMN_ENCLOSURE_URL = "enclosure_url";
+    // URI of the downloaded media, stored as a String
+    public static final String COLUMN_DOWNLOADED_MEDIA_URI = "download_media_uri";
+    // Reference to the relayed Channel, stored as a String
+    public static final String COLUMN_CHANNEL_ID = "channel_id";
+
+    public static Uri buildEpisodeURI(long id) {
+      return ContentUris.withAppendedId(CONTENT_URI, id);
+    }
   }
 }
